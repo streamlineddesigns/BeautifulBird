bb5b092ce1a28af5ac9682389492113a12b3bd6d
pierce <pierce.prange@protonmail.com>
2019-06-01 14:32:17 -0400

    test 5

--- Assets/Scripts/Demo/Demo.cs
+++ Assets/Scripts/Demo/Demo.cs
@@ -43,12 +43,51 @@ public class Demo : MonoBehaviour {
 		pitchValue = getCalculatedPitch();
 		yawValue = getCalculatedYaw();
 		rollValue = getCalculatedRoll();
-		DO.transform.rotation = Quaternion.Euler(pitchValue * -1.0f, yawValue, rollValue);
+
+		/*
+		//RIGHT Controller Perfect
+		pitchValue = RC.transform.eulerAngles.z;
+		yawValue = RC.transform.eulerAngles.y;
+		rollValue = RC.transform.eulerAngles.x;
+
+		DB.transform.eulerAngles = new Vector3(pitchValue, yawValue - 90.0f, -rollValue);
+		 */
+
+		/*
+		//LEFT Controller Perfect 
+		pitchValue = LC.transform.eulerAngles.z;
+		yawValue = LC.transform.eulerAngles.y;
+		rollValue = LC.transform.eulerAngles.x;
+
+		DB.transform.eulerAngles = new Vector3(-pitchValue, yawValue + 90.0f, rollValue);
+		
+		*/
+
+		
+		//Theoretically should work but does now.. without the division for averages
+		pitchValue = (RC.transform.eulerAngles.z + (LC.transform.eulerAngles.z * -1.0f));
+		yawValue = ((RC.transform.eulerAngles.y - 90.0f) + (LC.transform.eulerAngles.y + 90.0f));
+		rollValue = ((RC.transform.eulerAngles.x * -1.0f) + LC.transform.eulerAngles.x);
+
+		DB.transform.eulerAngles = new Vector3(pitchValue, yawValue, rollValue);
+		//DB.transform.rotation = Quaternion.Lerp(DB.transform.rotation, Quaternion.Euler(pitchValue, yawValue, rollValue), 0.05f); //Testing lerp
+		 
+
+		/*
+		//working example of both controllers
+		float pitchValue1 = RC.transform.eulerAngles.z;
+		float pitchValue2 = - LC.transform.eulerAngles.z;
+		float pitchValue3 = pitchValue1 + pitchValue2;
+		float pitchValue4 = pitchValue3 / 2;
+
+		DB.transform.eulerAngles = new Vector3(pitchValue4, 0.0f, 0.0f);
+		 */
+
 	}
 
 	protected float getCalculatedPitch()
 	{
-		float calculatedPitch = (RC.transform.localEulerAngles.z + (LC.transform.localEulerAngles.z * -1.0f)) / 2.0f;
+		float calculatedPitch = (RC.transform.eulerAngles.z + (LC.transform.eulerAngles.z * -1.0f)) / 2.0f;
 		return calculatedPitch;
 	}
 	protected float getCalculatedYaw()
